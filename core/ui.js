6a62e6ef803f7d89270df991ac6a3f933558a5b5
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-06-09 21:48:27 +0100

    minor update
    -icon added

--- core/ui.js
+++ core/ui.js
@@ -1,4 +1,9 @@
 var colorPicker = document.querySelector("#color-picker");
 Entity.prototype.color = colorPicker.value;
+
+function loadColor(){
+var colorPicker = document.querySelector("#color-picker");
+Entity.prototype.color = colorPicker.value;
 console.log(Entity);
 console.log(colorPicker.value);
+}
